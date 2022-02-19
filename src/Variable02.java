69ea3793444c6fd02d709623ed76c44b79f88d79
shunpii <shun.s0029@gmail.com>
2022-02-19 19:01:37 +0900

    [update]定数の学習

---
+++ src/Variable02.java
@@ -0,0 +1,14 @@
+import java.time.chrono.MinguoChronology;
+
+import javax.sound.midi.MidiChannel;
+
+public class Variable02 {
+    public static void main(String[] args) {
+        // 定数の宣言
+        final int MIN_NUMBER = 10;
+        System.out.println(MIN_NUMBER);
+    }
+
+    // 再代入しようとするとエラー
+    // MIN_NUMBER = 20;
+}
