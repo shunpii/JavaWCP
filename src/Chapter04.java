04b2268296da2cb6b1542edcd4de4e008ece0908
shunpii <shun.s0029@gmail.com>
2022-02-19 19:21:30 +0900

    [update]test問題

---
+++ src/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 7;
+        String str = "shunpii";
+        System.out.println(str);
+        str = "shun";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
