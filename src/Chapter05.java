d14cadf19f1104aa813768c4ad95d3dfa60156be
shunpii <shun.s0029@gmail.com>
2022-02-19 20:19:34 +0900

    [update]確認テスト

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+        long long1 = 9223372036854775807L;
+        System.out.println(long1);
+        double double1 = 1234567.89d;
+        System.out.println(double1);
+        boolean boolean1 = true;
+        System.out.println(boolean1);
+
+        Float float1 = 1.4142f;
+        System.out.println(float1);
+        Integer integer1 = 100;
+        System.out.println(integer1);
+        Character character1 = 'う';
+        System.out.println(character1);
+
+        int[] array = { 0, 10, 20, 30, 40};
+        System.out.println(array[3]);
+
+    }
+}
