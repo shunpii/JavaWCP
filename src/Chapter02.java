c47ec6abacd58dd4f71b68e1795f9bdade562041
shunpii <shun.s0029@gmail.com>
2022-02-19 18:25:08 +0900

    [update]

--- src/Chapter02.java
+++ src/Chapter02.java
@@ -1,6 +1,7 @@
 
 public class Chapter02 {
 	public static void main(String[] args) {
-		System.out.println("こんにちわ！");
+		System.out.println("Hello WEBVAMP!");
+		System.out.println("Hello Java!");
 	}
 }
