8ff5663485b393e27ae71ea04e79db491dabae76
shunpii <shun.s0029@gmail.com>
2022-02-20 15:13:48 +0900

    テスト完了

--- src/Chapter07.java
+++ src/Chapter07.java
@@ -13,17 +13,17 @@ public class Chapter07 {
 
         String color = "blue";
         switch (color) {
-        case "red":
-            System.out.println("赤信号です");
-            break;
-        case "yellow":
-            System.out.println("黄色信号です");
-            break;
-        case "blue":
-            System.out.println("青信号です");
-            break;
-        default:
-            System.out.println("信号の色ではありません");
+            case "red":
+                System.out.println("赤信号です");
+                break;
+            case "yellow":
+                System.out.println("黄色信号です");
+                break;
+            case "blue":
+                System.out.println("青信号です");
+                break;
+            default:
+                System.out.println("信号の色ではありません");
         }
     }
 }
