4246afa821d7985e8237417717c6711f3dd2e267
shunpii <shun.s0029@gmail.com>
2022-02-20 12:15:46 +0900

    switch文確認テスト完了

--- src/Chapter07.java
+++ src/Chapter07.java
@@ -10,5 +10,20 @@ public class Chapter07 {
         } else {
             System.out.println("とても遠いです");
         }
+
+        String color = "blue";
+        switch (color) {
+        case "red":
+            System.out.println("赤信号です");
+            break;
+        case "yellow":
+            System.out.println("黄色信号です");
+            break;
+        case "blue":
+            System.out.println("青信号です");
+            break;
+        default:
+            System.out.println("信号の色ではありません");
+        }
     }
 }
