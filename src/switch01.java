02242447cde9779033781cb5b327cbdbd370251b
shunpii <shun.s0029@gmail.com>
2022-02-20 11:46:31 +0900

    [update]switch文学習

---
+++ src/switch01.java
@@ -0,0 +1,15 @@
+public class switch01 {
+    public static void main(String[] args) {
+        int val = 2;
+        switch (val) {
+        case 3:
+            System.out.println("よくできました!");
+            break;
+        case 2:
+            System.out.println("ふつうでした!");
+            break;
+        case 1:
+            System.out.println("がんばろう!");
+        }
+    }
+}
